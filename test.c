d7a0a1581316da044e375d8931276a0ec6dc4936
Andreas Wien <andrewie@stud.ntnu.no>
2015-05-02 11:02:03 +0200

    Create test.c

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//#include <F:/nRF51 SDK/components/softdevice/s110/headers/include_all.h>
+//#include <nrf_delay.h>
+int main(int argc, const char* argv[]) {
+	while (1){
+		printf("Hello, World");
+		//nrf_delay_ms(500);
+		break;
+	}
+  return 0;
+}
